22ff97481e9f6ac1affeb06d5cca40d989d93b77
JULIE NORSETH <julienorseth@gmail.com>
2022-09-17 20:35:05 -0600

    Personal 01, working Chris Norseth

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Chris Norseth!')
+  res.send('Chris Norseth')
 })
 
 app.listen(port, () => {
