1a56506c1fe5cd8f3c458581439ae65c3e560196
JULIE NORSETH <julienorseth@gmail.com>
2022-09-24 21:50:18 -0600

    03 first attempt

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -22,4 +22,64 @@ const getSingle = async (req, res, next) => {
   });
 };
 
+const  createContact = async (req, res, next) => {
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+      };
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection()
+      .insertOne(contact);
+    if (response.acknowledged) {
+        res.status(201).json(response);
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+};
+
+const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .replaceOne({ _id: userId }, contact);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }
+  };
 module.exports = { getAll, getSingle };
+
+const deleteContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    }
+  };
+  
+  module.exports = {
+    getAll,
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
+  };
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,19 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+
+//read contacts
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
+
+// create contact
+router.post('/', contactsController.createContact);
+
+//update contact
+router.put('/:id', contactsController.updateContact);
+
+//delete contact
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
